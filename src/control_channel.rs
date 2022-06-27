ae62f96e7e74898a57e15a9861639a29f26d83ba
devensiv <devensiv@gmail.com>
2022-06-28 01:00:39 +0200

    remove unused use and format

--- src/control_channel.rs
+++ src/control_channel.rs
@@ -1,17 +1,20 @@
-use std::io::{self, ErrorKind, Write, BufWriter};
+use std::io;
 use std::net::SocketAddr;
-use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader};
+use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
 use tokio_rustls::{server::TlsStream, TlsAcceptor};
 
 pub async fn handle(socket: (TcpStream, SocketAddr), acceptor: TlsAcceptor) -> io::Result<()> {
-
     let mut _tls = rhizome_handshake(socket.0, &socket.1, acceptor).await?;
     println!("tls end: {}", socket.1);
     Ok(())
 }
 
-async fn rhizome_handshake<T>(stream: T, addr: &SocketAddr, acceptor: TlsAcceptor) -> io::Result<BufReader<TlsStream<T>>>
+async fn rhizome_handshake<T>(
+    stream: T,
+    addr: &SocketAddr,
+    acceptor: TlsAcceptor,
+) -> io::Result<BufReader<TlsStream<T>>>
 where
     T: AsyncRead + AsyncWrite + Unpin,
 {
@@ -30,6 +33,6 @@ where
     // Send rhizome hello
     tls.write_all(concat!("rhizome v", env!("CARGO_PKG_VERSION"), '\n').as_bytes())
         .await?;
-    
+
     Ok(BufReader::new(tls))
 }
