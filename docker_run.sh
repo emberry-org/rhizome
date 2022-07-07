066eced09c73d753af13ec6b6d406f49b2a1cdf3
devensiv <devensiv@gmail.com>
2022-07-07 08:43:30 +0200

    docker ignore better docker run

--- docker_run.sh
+++ docker_run.sh
@@ -8,6 +8,17 @@ if ! [ -f "server.key" ]; then
     exit
 fi
 
-cargo build --release
+echo "making sure $HOME/.cargo/registry exists"
+echo " --> mkdir $HOME/.cargo/registry -p"
+mkdir $HOME/.cargo/registry -p
+echo "run rust container to generate a Dockerfile compatible binary"
+echo " --> docker run --rm --user \"$(id -u)\":\"$(id -g)\" -it -v \"$HOME/.cargo/registry\":/usr/local/cargo/registry \ -v \"$(pwd)\":/usr/src/myapp -w /usr/src/myapp rust cargo build --release"
+docker run --rm --user "$(id -u)":"$(id -g)" -it -v "$HOME/.cargo/registry":/usr/local/cargo/registry \
+-v "$(pwd)":/usr/src/myapp -w /usr/src/myapp rust cargo build --release
+
+echo "build rhizome image"
+echo " --> docker build . -t rhizome"
 docker build . -t rhizome
+echo "run rhizome with default ports, name=rhizome, autorestart"
+echo " --> docker run --name rhizome --restart unless-stopped -dp 9999:9999 -p 10000:10000/udp rhizome"
 docker run --name rhizome --restart unless-stopped -dp 9999:9999 -p 10000:10000/udp rhizome
